a1e5cc0b39ce30d3de3e0725dd8ef6f68550f7be
SukritiR29 <sukritirajora@gmail.com>
2023-11-04 12:29:12 +0530

    newbg

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
-<template>
-  <div class="w-screen bg-weather-bg">
-
+  <template>
+    <div className="w-screen bg-opacity-20"  >
+      <div class="flex flex-col min-h-screen font-Roboto">
+      <navbar/> 
+      <RouterView/>
+  
+  
+    </div>
   </div>
-</template>
-
-<script setup>
-
-
-import weatherBackground from '@/assets/weather-bg.png';
-</script>
+  </template>
+  
+  <script setup>
+    import navbar from "./components/navbar.vue";
+    import { RouterView } from "vue-router";
+  </script>
 
 <style>
 .w-screen {
-  background-image: url('@/assets/weather-bg.png');
+  background-image: url('../src/assets/weather-bg.png'); /* Update the path if needed */
   background-size: cover;
   background-position: center;
-
+  /* Add other styles as needed */
 }
 </style>
