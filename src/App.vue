d571064498f9bdaaf2f260a37ede3bed3c53ceba
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2024-01-07 19:48:12 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,27 @@
-<template>
-  <div class="lg-w-screen lg-h-screen  relative">
-    <div class="w-screen" style="position: absolute; filter:blur(3vh);"></div>
-    <div class="flex flex-col min-h-screen font-Roboto relative">
-      <navbar class="" />
-      <RouterView />
+  <template>
+    <div className="lg-w-screen bg-emerald-950"  >
+      <div class="flex flex-col min-h-screen font-Roboto bg-emerald-950">
+      <navbar/> 
+      <RouterView/>
+  
+  
     </div>
   </div>
-</template>
-
-<script setup>
-import navbar from "./components/navbar.vue";
-import { RouterView } from "vue-router";
-</script>
+  </template>
+  
+  <script setup>
+    import navbar from "./components/navbar.vue";
+    import { RouterView } from "vue-router";
+  </script>
 
 <style>
 .w-screen {
-  position: absolute;
-  background-image: url('../src/assets/weather-bg.png');
+  background-image: url('../src/assets/weather-bg.png'); 
   background-size: cover;
   background-position: center;
   height: 100vh;
-  
+  margin: 0; 
+
 }
 
-/* Add a pseudo-element for the blurred effect behind the navbar */
-.w-screen::before {
-  content: '';
-  position: absolute;
-  background-image: inherit;
-  background-size: cover;
-  background-position: center;
-  filter: blur(10px);
-   /* Adjust the blur intensity */
-  
-}
-
-/* Ensure the navbar is positioned on top of the blurred background */
-navbar {
-  position: relative;
-  
-  backdrop-filter: blur(10px); /* Adjust the blur intensity */
-}
 </style>
