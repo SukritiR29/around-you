f5c889c82f391362639e7ef7cf728e1088e7159d
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2024-01-07 19:50:15 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
   <template>
-    <div className="lg-w-screen bg-emerald-950"  >
-      <div class="flex flex-col min-h-screen font-Roboto bg-emerald-950">
-      <navbar/> 
-      <RouterView/>
+  <div class="lg-w-screen lg-h-screen  relative">
+    <div class="w-screen" style="position: absolute; filter:blur(3vh);"></div>
+    <div class="flex flex-col min-h-screen font-Roboto relative">
+      <navbar class="" />
+      <RouterView />
   
   
     </div>
